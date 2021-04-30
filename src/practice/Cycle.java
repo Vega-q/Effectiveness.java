20500f2c55876a1f17729ae590ab0e148f6f75c0
Vega-q <1472813100@qq.com>
2021-04-30 19:26:19 +0800

    hello

--- src/practice/Cycle.java
+++ src/practice/Cycle.java
@@ -5,23 +5,32 @@ public class Cycle {
         System.out.println("Cycle");
     }
         public static void wheels(){
-            System.out.println("轮子数量是"+"num");
+            System.out.println("Cycle轮子数量是"+"num");
         }
     }
  class Unicycle extends Cycle{
     public void play(){
-
         System.out.println("Unicycle");
     }
+     public static void wheels(){
+         System.out.println("Unicycle轮子数量是"+"1");
+     }
 }
 class Bicycle extends Cycle {
     public void play(){
+
         System.out.println("Bicycle");
+    }
+    public static void wheels(){
+        System.out.println("Bicycle轮子数量是"+"2");
     }
 }
 class Tricycle extends Cycle {
     public void play(){
         System.out.println("Tricycle");
+    }
+    public static void wheels(){
+        System.out.println("Tricycle轮子数量是"+"3");
     }
 }
 

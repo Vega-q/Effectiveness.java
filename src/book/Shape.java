bc2d5ed56f7c852ac976924d077022c6d10c42a1
Vega-q <1472813100@qq.com>
2021-05-11 22:01:27 +0800

    hello

--- src/book/Shape.java
+++ src/book/Shape.java
@@ -1,21 +1,54 @@
 package book;
 
-abstract class Shape {
-    public abstract double area();
-    public abstract double perimeter(); }
-    class Circle extends Shape {
-    private double r;// 半径
-         public Circle(double r) { this.r = r; }
-         public double perimeter() { return 2*Math.PI*r; }
-         public double area() { return Math.PI*r*r; } }
-         class Rectangle extends Shape{ private double side1;  //第一条边
-              private double side2;  //第二条边
-              public Rectangle(double s1, double s2){ side1=s1; side2=s2; }
-              public double perimeter(){ return 2*(side1+side2); }
-              public double area(){ return side1*side2; } }
-              class TestPoly { public static void main(String[] args)
-              { Shape s1=new Circle(10);
-              System.out.println("圆形面积为:"+s1.area()); System.out.println("圆形周长为:"+s1.perimeter());
-              Shape s2=new Rectangle(2, 3); System.out.println("长方形面积为:"+s2.area());
-              System.out.println("长方形周长为:"+s2.perimeter()); } }
+public abstract class Shape {
+    double dim;
+    final double PI=3.14;
+    public Shape(double dim) {
+        this.dim=dim;
     }
+    public abstract double Area();//抽象方法，获得面积
+    public abstract double Perimeter();//抽象方法，获得周长
+}
+class Circle extends Shape{
+    public Circle(double dim) {
+        super(dim);
+    }
+    public double Area() {  //实现抽象方法Area()
+        return PI*dim*dim;
+    }
+    public double Perimeter() {  //实现抽象方法Perimeter()
+        return 2*PI*dim;
+    }
+}
+class Rectangle extends Shape {
+    double width;
+    public Rectangle(double dim,double width){
+        super(dim);
+        this.width=width;
+    }
+    public double  Area () {
+        return dim*width;
+    }
+    public double Perimeter() {
+        return 2*(dim+width);
+    }
+
+}
+class Test {
+
+    public static void main(String[] args) {
+        Shape shape =new Circle(10);
+        //声明一个Shape对象，指向实现它的子类对象
+        System.out.println("圆的面积："+shape.Area());
+        //调用Area()求圆的面积，并输出
+        System.out.println("圆的周长："+shape.Perimeter());
+        //调用Perimeter()求圆的周长，并输出
+        Shape rect1=new Rectangle(2,5);
+        System.out.println("长方形的面积是："+rect1.Area ());
+        System.out.println("长方形的周长是："+rect1.Perimeter());
+
+    }
+
+}
+
+
